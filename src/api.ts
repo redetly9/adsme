c71664fadb6e7aa0d20dc53138a1518b21da70b9
Almishev Nurbol <almishev.01@gmail.com>
2024-03-16 17:49:16 +0500

    render

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: 'https://adsme.app/',
+    baseURL: 'https://adsme-back.onrender.com/',
     headers: {'X-Custom-Header': 'foobar'}
   });
   
