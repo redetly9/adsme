ca010c08b8857395ff14019ede5f9756de799d46
nurbol <nurbol@geex-arts.com>
2024-03-31 14:41:17 +0500

    upd

--- src/templates/create-post/App.tsx
+++ src/templates/create-post/App.tsx
@@ -11,78 +11,78 @@ import CreatePost from './CreatePost';
 
 export default function PostList() {
 
-  const [posts, setPosts] = React.useState(null)
-  const [radius, setRadius] = React.useState(null)
-  const { latitude, longitude } = useAppSelector(state => state.user.geo)
+  // const [posts, setPosts] = React.useState(null)
+  // const [radius, setRadius] = React.useState(null)
+  // const { latitude, longitude } = useAppSelector(state => state.user.geo)
 
-  const marks = [
-    {
-      value: 0,
-      label: '0',
-    },
-    {
-      value: 100,
-      label: '',
-    },
-    {
-      value: 200,
-      label: '',
-    },
-    {
-      value: 300,
-      label: '',
-    },
-    {
-      value: 400,
-      label: '',
-    },
-    {
-      value: 500,
-      label: '',
-    },
-    {
-      value: 600,
-      label: '',
-    },
-    {
-      value: 700,
-      label: '',
-    },
-    {
-      value: 800,
-      label: '',
-    },
-    {
-      value: 900,
-      label: '',
-    },
-    {
-      value: 1000,
-      label: '1000',
-    },
-  ];
+  // const marks = [
+  //   {
+  //     value: 0,
+  //     label: '0',
+  //   },
+  //   {
+  //     value: 100,
+  //     label: '',
+  //   },
+  //   {
+  //     value: 200,
+  //     label: '',
+  //   },
+  //   {
+  //     value: 300,
+  //     label: '',
+  //   },
+  //   {
+  //     value: 400,
+  //     label: '',
+  //   },
+  //   {
+  //     value: 500,
+  //     label: '',
+  //   },
+  //   {
+  //     value: 600,
+  //     label: '',
+  //   },
+  //   {
+  //     value: 700,
+  //     label: '',
+  //   },
+  //   {
+  //     value: 800,
+  //     label: '',
+  //   },
+  //   {
+  //     value: 900,
+  //     label: '',
+  //   },
+  //   {
+  //     value: 1000,
+  //     label: '1000',
+  //   },
+  // ];
 
-  function valueText(value: number) {
-    return `${value}`;
-  }
+  // function valueText(value: number) {
+  //   return `${value}`;
+  // }
 
-  const getPosts = async () => {
-    const { data } = await api.get('v2/posts', {
-      params: {
-        latitude,
-        longitude,
-        radius,
-      }
-    })
+  // const getPosts = async () => {
+  //   const { data } = await api.get('v2/posts', {
+  //     params: {
+  //       latitude,
+  //       longitude,
+  //       radius,
+  //     }
+  //   })
 
-    setPosts(data.slice().reverse())
-  }
+  //   setPosts(data.slice().reverse())
+  // }
 
-  React.useEffect(() => {
-    if (latitude && longitude) {
-      getPosts()
-    }
-  }, [latitude, longitude, radius])
+  // React.useEffect(() => {
+  //   if (latitude && longitude) {
+  //     getPosts()
+  //   }
+  // }, [latitude, longitude, radius])
 
 
   return (
