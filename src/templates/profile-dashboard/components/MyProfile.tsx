0270c040c253b1c82363a04b9de9e21b0e84931a
nurbol <nurbol@geex-arts.com>
2024-05-29 23:58:50 +0500

    test svg

--- src/templates/profile-dashboard/components/MyProfile.tsx
+++ src/templates/profile-dashboard/components/MyProfile.tsx
@@ -296,15 +296,43 @@ const [open, setOpen] = React.useState(false);
             <FormControl>
               <FormLabel>Номер телефона</FormLabel>
               <div>{formattedPhoneNumber}</div>
-
+              <Divider></Divider>
 
               
-{profileData ? (    <Button onClick={()=> {
+{/* {profileData ? (    <Button onClick={()=> {
                 if(profileData?.activate === false){
                   alert('netu')
                 }
                 else {navigate('/post/create')}
-                }}>1</Button>): ('')}
+                }}>1</Button>): ('')} */}
+
+                <Box sx={{marginTop:'16px'}}>
+                <FormLabel>Настройки</FormLabel>
+
+                  <Box> 
+                  {profileData ? (       <Box sx={{display:'flex', alignItems:'center', gap:'10px', marginBottom:'6px'}}
+                        onClick={()=> {
+                          if(profileData?.activate === false){
+                            alert('netu')
+                          }
+                          else {navigate('/post/create')}
+                          }}>
+                          <Box sx={{
+                            // backgroundColor:'#f7f7f7', 
+                            boxShadow:'initial', borderRadius:'30px', display:'flex', justifyContent:'center', alignItems:'center', width:'30px', height:'30px'
+                            }}>              
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M18 20H4V6h9V4H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-9h-2v9zm-7.79-3.17l-1.96-2.36L5.5 18h11l-3.54-4.71zM20 4V1h-2v3h-3c.01.01 0 2 0 2h3v2.99c.01.01 2 0 2 0V6h3V4h-3z"/></svg>
+                            {/* <EditRoundedIcon /> */}
+                            </Box>
+                          <Box>Создать пост</Box>
+                          
+                        </Box>): ('')}
+                        <Divider></Divider>
+
+                        <Box></Box>
+                  </Box>
+
+                </Box>
           
 
 
