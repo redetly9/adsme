6d97cb2ae9ce1d281b9bec169ebeaed0b144dbc2
Anton <spinsib@gmail.com>
2024-07-17 20:25:18 +0300

    исправление сайдбара

--- src/components/navbar/ui/index.tsx
+++ src/components/navbar/ui/index.tsx
@@ -3,24 +3,42 @@ import Tab, { tabClasses } from '@mui/joy/Tab'
 import TabList from '@mui/joy/TabList'
 import Tabs from '@mui/joy/Tabs'
 import * as React from 'react'
-import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { useEffect, useMemo, useState } from 'react'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 
 import { createNavbarTabs } from '../consts'
 
 export const Navbar = () => {
-  const TAB_INDEX_LOCAL_STORAGE_KEY = 'tabIndex'
-  const lsTabIndex = localStorage.getItem(TAB_INDEX_LOCAL_STORAGE_KEY)
-  const [tabIndex, setTabIndex] = useState(lsTabIndex ? +lsTabIndex : 1)
+  /**
+   * State
+   * */
+  const [tabIndex, setTabIndex] = useState(0)
+  const location = useLocation()
+  const navigate = useNavigate()
+  /**
+   * Constants
+   * */
   const id = localStorage.user
-  const colors = ['primary', 'primary', 'primary', 'primary'] as const
+  const tabs = useMemo(() => createNavbarTabs(id), [id])
 
   const handleChange = (_: React.SyntheticEvent | null, value: number | string | null) => {
-    if (!value) return
-
+    if (value === null) return
     setTabIndex(+value)
-    localStorage.setItem(TAB_INDEX_LOCAL_STORAGE_KEY, value.toString())
   }
+
+  // Костыльное решение, потом поменяем архитектуру
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate(tabs[1].path)
+    }
+  }, [location, navigate, tabs])
+
+  useEffect(() => {
+    const activeTab = tabs.findIndex(tab => tab.path === location.pathname)
+    if (activeTab !== -1) {
+      setTabIndex(activeTab)
+    }
+  }, [location, id, tabs])
 
   return (
     <Box>
@@ -36,7 +54,7 @@ export const Navbar = () => {
           overflow: 'hidden',
           mx: 'auto',
           boxShadow: theme.shadow.sm,
-          '--joy-shadowChannel': theme.vars.palette[colors[0]].darkChannel,
+          '--joy-shadowChannel': theme.vars.palette.primary.darkChannel,
           [`& .${tabClasses.root}`]: {
             py: 1,
             flex: 1,
@@ -56,29 +74,24 @@ export const Navbar = () => {
           sx={{ borderRadius: 'lg', p: 0 }}
         >
           {
-            createNavbarTabs(id).map(({ icon, label, path }, index) => (
+            tabs.map(({ icon, label, path }) => (
               <NavLink
                 key={path + '_link'}
                 to={path}
                 style={{ width: '25%', maxWidth: '25%' }}
               >
-                {({ isActive }) => {
-                  if (isActive) {
-                    setTabIndex(prev => prev !== index ? index : prev)
-                  }
-                  return (
-                    <Tab
-                      key={path + '_tab'}
-                      disableIndicator
-                      orientation='vertical'
-                      sx={{ width: '100%' }}
-                      color={isActive ? 'primary' : undefined}
-                    >
-                      {icon}
-                      {label}
-                    </Tab>
-                  )
-                }}
+                {({ isActive }) => (
+                  <Tab
+                    key={path + '_tab'}
+                    disableIndicator
+                    orientation='vertical'
+                    sx={{ width: '100%' }}
+                    color={isActive ? 'primary' : undefined}
+                  >
+                    {icon}
+                    {label}
+                  </Tab>
+                )}
               </NavLink>
             ))
           }
