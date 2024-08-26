51011e4490dd4ecdd8142b7ff86501709237a3df
Anton <spinsib@gmail.com>
2024-08-26 16:39:13 +0300

    fixes

--- src/pages/user-chat-page/components/user-chat-page/ui/index.tsx
+++ src/pages/user-chat-page/components/user-chat-page/ui/index.tsx
@@ -19,7 +19,9 @@ export const UserChatPage = memo(({ isGroupChat }: UserChatPageProps) => {
   const user = useUserStore(state => state.user)
   const [chatMessages, setChatMessages] = useState<ChatParticipantsType[] | null>(null)
   const [chat, setChat] = useState<ChatParticipantsType[] | null>(null)
-  const [textAreaValue, setTextAreaValue] = useState('')
+  // const [textAreaValue, setTextAreaValue] = useState('')
+
+  console.log(chatMessages)
 
   const chatId = useMemo(() => isGroupChat ? '58' : paramsChatId, [paramsChatId, isGroupChat])
   const sender = chat?.find(c => c.user_profile_id !== user?.id)?.user_profiles
