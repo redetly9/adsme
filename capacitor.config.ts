9dd6be6fa33a51afb81b9ab621c636a4ba6d7d5b
Anton <spinsib@gmail.com>
2024-08-31 14:15:50 +0300

    добавил сборку андроида

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'app.adsme.apps',
+  appName: 'Adsme',
+  webDir: 'dist'
+}
+
+export default config
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 8080
+    },
+    resolve: {
+        alias: {
+            '~app': path.resolve('src/app'),
+            '~pages': path.resolve('src/pages'),
+            '~layouts': path.resolve('src/layouts'),
+            '~components': path.resolve('src/components'),
+            '~model': path.resolve('src/model'),
+            '~shared': path.resolve('src/shared')
+        }
+    }
+});
