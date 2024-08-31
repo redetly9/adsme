d389c590605a9fc9ac795cb16251a8ca95bd7511
Anton <spinsib@gmail.com>
2024-08-31 13:50:09 +0300

    маленькие разборки с capcator

--- android/app/src/main/java/app/adsme/apps/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package app.adsme.apps;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli'
-
-const config: CapacitorConfig = {
-  appId: 'app.adsme.apps',
-  appName: 'Adsme',
-  webDir: 'dist'
-}
-
-export default config
